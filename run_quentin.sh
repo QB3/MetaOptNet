f555d441be9d3f1510012abfead0c98e5f550928
QB3 <quentin.bertrand@inria.fr>
2022-11-15 15:57:15 -0500

    added run_quentin script

---
+++ run_quentin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --time=3:00:00
+#SBATCH --partition=long
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --exclude=cn-e001,cn-e002,cn-e003
+
+# SBATCH --output=tmp/slurm/slurm-%j.out
+
+#SBATCH --output=job_output.txt
+#SBATCH --error=job_error.txt
+
+# source ~/venv38bis/bin/activate
+
+module load anaconda/3
+conda activate torchopt
+
+python train.py --save-path "/home/mila/q/quentin.bertrand/MetaOptNet/" --train-shot 15 --head SparseLogReg --network ProtoNet --dataset miniImageNet --eps 0.1
