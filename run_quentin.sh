038eff8b752cb4462a3f087fc47a1c2d4a917d7c
QB3 <quentin.bertrand@inria.fr>
2023-03-01 10:45:57 -0500

    modif running file

--- run_quentin.sh
+++ run_quentin.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-#SBATCH --time=24:00:00
+#SBATCH --time=26:00:00
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
-#SBATCH --exclude=cn-e001,cn-e002,cn-e003
+
+# SBATCH --exclude=cn-e001,cn-e002,cn-e003,kepler[3-5]
 
 # SBATCH --output=tmp/slurm/slurm-%j.out
 
@@ -19,7 +20,8 @@ conda activate torchopt
 # python train.py --gpu 0 --save-path "/home/mila/q/quentin.bertrand/MetaOptNet/Sparse-SVM-ResNet/" --train-shot 15 --head Sparse-SVM --network ResNet --dataset miniImageNet --eps 0.1
 # python train.py --gpu 0 --save-path "/home/mila/q/quentin.bertrand/MetaOptNet/SVM-ResNet/" --train-shot 15 --head SVM --network ResNet --dataset miniImageNet --eps 0.1
 # WANDB_MODE=dryrun
-python train.py --gpu 0 --train-shot 15 --head Sparse-SVM --network ProtoNet --dataset miniImageNet --eps 0.1
+python train.py --train-shot 15 --head Sparse-SVM --network ProtoNet --dataset miniImageNet --eps 0.1 --dual_reg 100 --lambda2 1000.0 --lambda1 0.0
+# python train.py --train-shot 15 --head Sparse-SVM --network ProtoNet --dataset miniImageNet --eps 0.1 --dual_reg 1.0 --lambda2 1000.0 --lambda1 0.0
 # python train.py --gpu 0 --save-path "/home/mila/q/quentin.bertrand/MetaOptNet/Sparse-SVM/" --train-shot 15 --head Sparse-SVM --network ProtoNet --dataset miniImageNet --eps 0.1
 # python train.py --gpu 0 --save-path "/home/mila/q/quentin.bertrand/MetaOptNet/PROTOTYPE/" --train-shot 15 --head Sparse-SVM --network ProtoNet --dataset miniImageNet --eps 0.1
 # python train.py --gpu 0 --save-path "/home/mila/q/quentin.bertrand/MetaOptNet/" --train-shot 15 --head Sparse-SVM --network ProtoNet --dataset miniImageNet --eps 0.1
